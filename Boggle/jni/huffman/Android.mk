8738c31b9b1d1b136aac2df4dffd0b581fe38563
bin bo <bigbugbb@gmail.com>
2012-10-09 18:28:46 -0400

    Change Dir

--- Boggle/jni/huffman/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE    := huffman
-LOCAL_SRC_FILES := huffman.c
-
-include $(BUILD_SHARED_LIBRARY)
--- Boggle/obj/local/armeabi/objs/dictionary/Utinities/Event.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/dictionary/Utinities/Event.o: \
- jni/dictionary/Utinities/Event.cpp \
- /Developer/Android/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/iostream
-
-/Developer/Android/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/iostream:
