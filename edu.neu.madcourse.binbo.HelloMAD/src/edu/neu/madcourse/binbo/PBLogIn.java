7c7bd8888679f5b39bcef54d90101229a7f40ff6
bin bo <bigbugbb@gmail.com>
2012-10-27 16:10:23 -0400

    login signup main, pass the current player info to invite

--- edu.neu.madcourse.binbo.HelloMAD/src/edu/neu/madcourse/binbo/PBLogIn.java
+++ edu.neu.madcourse.binbo.HelloMAD/src/edu/neu/madcourse/binbo/PBLogIn.java
@@ -1,14 +1,10 @@
 package edu.neu.madcourse.binbo;
 
-import java.util.ArrayList;
 import java.util.Date;
 import org.json.JSONException;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.Message;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.EditText;
@@ -16,84 +12,9 @@ import android.widget.Toast;
 
 public class PBLogIn extends Activity implements OnClickListener {
 	private EditText mEditTextAccount = null;
-	private PBNameList   mNames   = new PBNameList();
-	private AcquireTask  mAcquire = null;
-	private static final String ACCOUNT_NAME = "account_name";
-	
-	private final Handler mHandler = new Handler(Looper.getMainLooper()) {
-		
-		private static final int SERVER_UNAVAILABLE = -1;
-		private static final int UPDATE_DATA_DONE   = 1;  
-		private static final int UPDATE_DATA_ERROR  = 2;
-		private static final int COMMIT_DATA_DONE   = 3; 
-	    private static final int COMMIT_DATA_ERROR  = 4;
-        	
-    	public void handleMessage(Message msg) {        	
-        	
-        	switch (msg.arg1) { 
-    		case SERVER_UNAVAILABLE:
-    			onServerUnavailable();
-    			break;
-            case UPDATE_DATA_DONE:
-            	onUpdateDataDone();
-                break;   
-            case UPDATE_DATA_ERROR:
-            	onUpdateDataError();
-            	break;
-            case COMMIT_DATA_DONE:
-            	onCommitDataDone();
-            	break;
-            case COMMIT_DATA_ERROR:
-            	onCommitDataError();
-            	break;
-            default:
-            	break;
-            }            
-        } 
-
-    };
-    
-    private void onServerUnavailable() {
-    	Toast.makeText(getApplicationContext(), 
-            "Sorry, server can not be connected. Please try again.",
-            Toast.LENGTH_LONG).show();
-    }
-    
-    private void onUpdateDataDone() {    	
-    	String account = mEditTextAccount.getText().toString();    			
-		boolean found = false;
-		
-    	for (int i = 0; i < mNames.size(); i++) {
-    		String existAccount = mNames.get(i);
-    		if (existAccount.compareTo(account) == 0) { // if exists, then login
-    			found = true;
-    			break;
-    		}
-    	}
-    	
-    	if (found) {
-    		doLogin(account);
-    	} else {
-	    	Toast.makeText(getApplicationContext(), 
-               "Sorry, the account name doesn't exist. Please login first.",
-               Toast.LENGTH_LONG).show();	    	
-    	}    	
-    }       
-    
-    private void onUpdateDataError() {    	
-    	Toast.makeText(getApplicationContext(), 
-                "Sorry, the account name doesn't exist. Please login first.",
-                Toast.LENGTH_LONG).show();
-    }
-
-    private void onCommitDataDone() {
-  
-    }
-    
-    private void onCommitDataError() {
-
-    }
-	
+	private PBNameList mNames = new PBNameList();
+	private static final String ACCOUNT_NAME = "account_name";	
+     
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.pb_login);
@@ -105,40 +26,66 @@ public class PBLogIn extends Activity implements OnClickListener {
 		btn_login.setOnClickListener(this);
 	}
 	
-	public void onClick(View v) {
-		String account = mEditTextAccount.getText().toString();
-		
+	public void onClick(View v) {				
 		switch (v.getId()) {
 		case R.id.pblogin_login_button:
-			if (account.compareTo("") != 0) {
-				mAcquire = new AcquireTask(mHandler, mNames, 0, true);
-				mAcquire.start();
-			} else {
-				Toast.makeText(getApplicationContext(), 
-	                       "Please enter an account name.",
-	                       Toast.LENGTH_LONG).show();
-			}
+			doLogin();
 			break;
 		}
 	}
 	
-	public void doLogin(String account) {
-		PBPlayerInfo pInfo = new PBPlayerInfo(account);
-		try {
-			pInfo.acquire();
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		pInfo.setStatus("Online");
-		pInfo.setUpdateTime((new Date()).getTime());
-		CommitTask cmt = new CommitTask(this.mHandler, pInfo);
-		cmt.execute();
+	public void doLogin() {
+		String account = mEditTextAccount.getText().toString();
 		
-		Intent i = new Intent(this, PBMain.class);
-		i.putExtra(ACCOUNT_NAME, account);
-		finish();
-		startActivity(i);					
+		if (account.compareTo("") == 0) {
+			Toast.makeText(this, 
+				"Please enter an account name.", Toast.LENGTH_LONG).show();
+			return;
+		}
+		
+		PBPlayerInfo newPlayer = new PBPlayerInfo(account);
+		
+		try {
+			if (mNames.acquire() == false) {
+				Toast.makeText(this, 
+		            "Sorry, server can not be connected. Please try again.",
+		            Toast.LENGTH_LONG).show();
+				return;
+			} 
+			
+			boolean found = false;			
+	    	for (int i = 0; i < mNames.size(); i++) {
+	    		String existAccount = mNames.get(i);
+	    		if (existAccount.compareTo(account) == 0) { // if exists, then login
+	    			found = true;
+	    			break;
+	    		}
+	    	}
+	    	
+	    	if (found) {		    		
+	    		// not necessary to do this commit, but we do it now to prevent error 
+	    		newPlayer.setStatus("offline");
+	    		newPlayer.setUpdateTime((new Date()).getTime());
+	    		if (newPlayer.commit() == false) {
+	    			Toast.makeText(this, 
+    		            "Sorry, server can not be connected. Please try again.",
+    		            Toast.LENGTH_LONG).show();
+	    		} else {
+	    			Intent i = new Intent(this, PBMain.class);
+	    			i.putExtra(PBMain.HOST_INFO, newPlayer.obj2json());
+		    		finish();
+		    		startActivity(i);
+	    		}		    				    			
+	    	} else {
+		    	Toast.makeText(getApplicationContext(), 
+	               "Sorry, the account name doesn't exist. Please sign up first.",
+	               Toast.LENGTH_LONG).show();	  
+		    	return;
+	    	}    				
+		} catch (JSONException e) {
+			Toast.makeText(getApplicationContext(), 
+                "Sorry, the account name doesn't exist. Please login first.",
+                Toast.LENGTH_LONG).show();
+		}						
 	}
-
 }
--- edu.neu.madcourse.binbo.HelloMAD/src/edu/neu/madcourse/binbo/PBMain.java
+++ edu.neu.madcourse.binbo.HelloMAD/src/edu/neu/madcourse/binbo/PBMain.java
@@ -1,15 +1,9 @@
 package edu.neu.madcourse.binbo;
 
-import java.util.Date;
-
 import org.json.JSONException;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.Message;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
@@ -17,21 +11,24 @@ import android.widget.Toast;
 
 public class PBMain extends Activity implements OnClickListener {
 	private TextView mTextViewAccount;
-	private String mAccount = "";
-	private PBPlayerInfo mPlayer  = null;
-	private AcquireTask  mAcquire = null;
-	private CommitTask   mCommit  = null;
-	private static final String ACCOUNT_NAME = "account_name";
+	private PBPlayerInfo mPlayer  = new PBPlayerInfo("");
 	public static final String HOST_INFO = "host_info";
 	
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.pb_main);
 
-		Intent intent = getIntent();
-		mAccount = intent.getStringExtra(ACCOUNT_NAME);
+		Intent intent = getIntent();		
+		try {
+			mPlayer.json2obj(intent.getStringExtra(HOST_INFO));
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace(); 
+			// ignore this error
+		}
+		
 		mTextViewAccount = (TextView)findViewById(R.id.pbmain_account_name_label);
-		mTextViewAccount.setText("Hi, " + mAccount);
+		mTextViewAccount.setText("Hi, " + mPlayer.getName());
 		
 		// Set up click listeners for all the buttons
 		View btnNewGame = findViewById(R.id.pbmain_new_game_button);
@@ -44,9 +41,6 @@ public class PBMain extends Activity implements OnClickListener {
 		btnAbout.setOnClickListener(this);
 		View btnExit = findViewById(R.id.pbmain_exit_button);
 		btnExit.setOnClickListener(this);
-		
-		mPlayer = new PBPlayerInfo(mAccount);
-		mCommit = new CommitTask(mHandler, mPlayer);
 	}
 	
 	public void onClick(View v) {
@@ -61,113 +55,35 @@ public class PBMain extends Activity implements OnClickListener {
 	}
 	
 	private void startNewGame() {			
-		mAcquire = new AcquireTask(mHandler, mPlayer, 0, true);
-		mAcquire.start();		
+		try {
+			mPlayer.setStatus("online"); // update the status to online
+			mPlayer.setUpdateTime(System.currentTimeMillis());		
+			
+			Intent i = new Intent(this, PBInvite.class);
+			i.putExtra(PBMain.HOST_INFO, mPlayer.obj2json());
+    		startActivity(i);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			Toast.makeText(this, 
+               "Sorry, can't start the new game.", Toast.LENGTH_LONG).show();	  
+	    	return;
+		} 
 	}
 	
 	private void exitGame() {
-		this.mPlayer.setStatus("Offline");
-		this.mPlayer.setUpdateTime((new Date()).getTime());
-		CommitTask cmt = new CommitTask(this.mHandler, this.mPlayer);
-		cmt.execute();
-	}
-	
-	private final Handler mHandler = new Handler(Looper.getMainLooper()) {
+		mPlayer.setStatus("offline");
+		mPlayer.setUpdateTime(System.currentTimeMillis());		
 		
-		private static final int SERVER_UNAVAILABLE = -1;
-		private static final int UPDATE_DATA_DONE   = 1;  
-		private static final int UPDATE_DATA_ERROR  = 2;
-		private static final int COMMIT_DATA_DONE   = 3; 
-	    private static final int COMMIT_DATA_ERROR  = 4;
- 
-		public void handleMessage(Message msg) {        	
-        	
-        	switch (msg.arg1) { 
-    		case SERVER_UNAVAILABLE:
-    			onServerUnavailable();
-    			break;
-            case UPDATE_DATA_DONE:
-            	onUpdateDataDone();	            	
-                break;   
-            case UPDATE_DATA_ERROR:
-            	onUpdateDataError();
-            	break;
-            case COMMIT_DATA_DONE:
-            	onCommitDataDone();
-            	break;
-            case COMMIT_DATA_ERROR:
-            	onCommitDataError();
-            	break;
-            default:
-            	break;
-            }            
-        } 
+		try {
+			if (mPlayer.commit() == false) {
+				// do nothing because you don't want to bother the user
+			}
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		private void onServerUnavailable() {
-	    	Toast.makeText(getApplicationContext(), 
-	            "Sorry, server can not be connected. Please try again.",
-	            Toast.LENGTH_LONG).show();
-	    }
-	    
-	    private void onUpdateDataDone() {    	
-	    	//Intent i = new Intent(getApplicationContext(), PBGame.class);
-			Intent i = new Intent(getApplicationContext(), PBInvite.class);
-
-			//mPlayer.setStatus("online"); // update the status to online
-			//mPlayer.setUpdateTime(System.currentTimeMillis());
-			
-			//Bundle bundle = new Bundle();  
-		    //bundle.putBoolean("new game", true);		    
-		    //bundle.putSerializable(HOST_INFO, mPlayer); 
-		    //i.putExtras(bundle); 
-
-			try {
-				mPlayer.setStatus("online"); // update the status to online
-				mPlayer.setUpdateTime(System.currentTimeMillis());
-				i.putExtra(HOST_INFO, mPlayer.obj2json());		
-			} catch (JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} 
-
-
-			finish();	
-			startActivity(i);
-	    }       
-	    
-	    private void onUpdateDataError() {    	
-	    	// not necessary, already create player info when sign up and log in
-	    	
-	    	// no player info found, just commit a new one
-	    	//mPlayer.setScore(0);
-	    	//mPlayer.setBestScore(0);
-	    	//mPlayer.setSelLetters("");
-	    	//mPlayer.setStatus("online");
-	    	//mPlayer.setUpdateTime(System.currentTimeMillis());
-	    	
-	    	// create commit task
-			//mCommit.execute();
-	    }
-
-	    private void onCommitDataDone() {
-	    	//Intent i = new Intent(getApplicationContext(), PBInvite.class);
-	    	
-	    	//Bundle bundle = new Bundle();  
-		    //bundle.putBoolean("new game", true);		    
-		    //bundle.putSerializable(HOST_INFO, mPlayer); 
-		    //i.putExtras(bundle);
-		    
-			//startActivity(i);
-
-			Intent i = new Intent(getApplicationContext(), HelloMAD.class);
-	    	finish();
-	    	startActivity(i);
-	    }
-	    
-	    private void onCommitDataError() {
-	    	Toast.makeText(getApplicationContext(), 
-	            "Fail to start the game. Please try again.",
-	            Toast.LENGTH_LONG).show();
-	    }
-    };
+		finish(); // just close the activity	
+	}		
 }
