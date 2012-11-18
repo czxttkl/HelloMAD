53dfdf279756030413c39b533f027f4b35a2d453
unknown <wwwjdan@hotmail.com>
2012-11-18 14:07:03 -0500

    add file - wwj

---
+++ edu.neu.madcourse.binbo.HelloMAD/src/edu/neu/madcourse/binbo/rocketrush/gameobjects/ZOrders.java
@@ -0,0 +1,9 @@
+package edu.neu.madcourse.binbo.rocketrush.gameobjects;
+
+public class ZOrders {
+	public static final int BACKGROUND_FAR  = 0;
+	public static final int BACKGROUND_NEAR = 1;
+	public static final int ASTEROID		= 2;
+	public static final int ROCKET			= 3;
+	public static final int SPEEDBAR		= 4;
+}
