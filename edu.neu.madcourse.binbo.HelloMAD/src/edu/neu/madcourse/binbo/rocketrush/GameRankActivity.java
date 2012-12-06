f4854e836590351c79f56d10f8e0f5b80db13a9f
bin bo <bigbugbb@gmail.com>
2012-12-05 21:29:17 -0500

    +

---
+++ edu.neu.madcourse.binbo.HelloMAD/src/edu/neu/madcourse/binbo/rocketrush/GameRankActivity.java
@@ -0,0 +1,97 @@
+package edu.neu.madcourse.binbo.rocketrush;
+
+import java.util.List;
+
+import edu.neu.madcourse.binbo.R;
+import edu.neu.madcourse.binbo.boggle.BogglePuzzleView;
+import edu.neu.madcourse.binbo.persistentboggle.PBPlayerInfo;
+import android.graphics.Color;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+public class GameRankActivity extends FragmentActivity implements OnClickListener {
+	
+	protected TableLayout mTable = null;
+	protected Button mBackButton = null;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+        setContentView(R.layout.rocket_rush_rank);
+        // get views and set listeners
+		setupViews();
+		// adjust layouts according to the screen resolution
+		adjustLayout();        
+	}
+	
+	private void setupViews() {
+		mTable = (TableLayout) findViewById(R.id.tableLayoutRocketRank);
+		// Set up click listeners for all the buttons
+		mBackButton = (Button) findViewById(R.id.rocketRankButtonBack);		
+		mBackButton.setOnClickListener(this);
+	}
+	
+	private void adjustLayout() {
+		getWindow().setFlags(
+			WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON | WindowManager.LayoutParams.FLAG_FULLSCREEN, 
+			WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON | WindowManager.LayoutParams.FLAG_FULLSCREEN
+		); 
+		// get screen resolution
+		DisplayMetrics dm = new DisplayMetrics();  
+        Display display = getWindowManager().getDefaultDisplay(); 		
+        display.getMetrics(dm);        
+        // adjust the layout according to the screen resolution
+// 		LinearLayout main = (LinearLayout)findViewById(R.id.rocketrankRoot);				   
+		LayoutParams laParams = null;
+		ScrollView scrollView = (ScrollView)findViewById(R.id.rocketrankScrollView);
+		laParams = scrollView.getLayoutParams();
+		laParams.height = (int) (dm.heightPixels * 0.6f);
+		scrollView.setLayoutParams(laParams);
+	}
+
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.rocketRankButtonBack:
+			finish();
+			break;
+		}
+	}
+
+	@Override
+	protected void onResume() {
+		for (int i = 0; i < 50; ++i) {    		
+	    	TableRow tablerow = new TableRow(getApplicationContext());  
+            tablerow.setBackgroundColor(Color.rgb(222, 220, 210));  
+                              
+                TextView textViewRank = new TextView(getApplicationContext());  
+                textViewRank.setText(String.valueOf(i + 1));                
+                tablerow.addView(textViewRank);   
+                
+//                TextView textViewScore = new TextView(getApplicationContext());  
+//                textViewName.setText(player.getName());                
+//                tablerow.addView(textViewName);
+//                
+//                TextView textViewTime = new TextView(getApplicationContext());  
+//                textViewBest.setText(String.valueOf(player.getBestScore()));                
+//                tablerow.addView(textViewBest);
+              
+            mTable.addView(tablerow, new TableLayout.LayoutParams(  
+                    LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
+    	}         
+		super.onResume();
+	}
+}
